e2b8da3ef9565a10c3c730febd1f37e043bd6836
Michael Mottola <mottola64@gmail.com>
2019-04-07 23:33:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,15 +8,11 @@ class Dog
     @@all << self
   end
   
-  def name
-    @name = name
-  end
-  
   def self.clear_all
     @@all.clear
     
   def self.all
     self.each do |dog|
-      
+      dog.name
 end
 end
